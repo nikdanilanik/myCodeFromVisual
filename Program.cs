3e4bb660d9223494bca58d5efdbb306586a3b748
nikdanilanik <90115416+nikdanilanik@users.noreply.github.com>
2022-11-24 15:58:37 +0300

    Uploaded
    Uploaded my code

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using MySqlX.XDevAPI.Common;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using static System.Net.Mime.MediaTypeNames;
+
+string snumber = Console.ReadLine();
+int number = Convert.ToInt32(snumber);
+
+switch (number)
+{
+    case 0:
+        //link = https://www.codewars.com/kata/514b92a657cdc65150000006
+        //string si = Console.ReadLine();
+        //int i = Convert.ToInt32(si);
+        int i = 1000;
+        Console.WriteLine(Main.Sol(i));
+        break;
+
+    case 1:
+        //link = https://www.codewars.com/kata/587731fda577b3d1b0001196
+        string st = Console.ReadLine();
+        Console.WriteLine(Main.Upper(st));
+        break;
+
+    case 2:
+        //link = https://www.codewars.com/kata/52742f58faf5485cae000b9a
+        string timeSecond = Console.ReadLine();
+        int itimeSecond = Convert.ToInt32(timeSecond);
+        int[] result = Main.Times.TimeScore(itimeSecond);
+        Console.Write(Environment.NewLine + $"input {timeSecond} second = ");
+        if (result[0] != 0) Console.Write($"{result[0]} day ");
+        if (result[1] != 0) Console.Write($"{result[1]} hour ");
+        if (result[2] != 0) Console.Write($"{result[2]} minute ");
+        if (result[3] != 0) Console.WriteLine($"{result[3]} second");
+        break;
+
+    case 3:
+        //link = https://www.codewars.com/kata/51b66044bce5799a7f000003
+        string Roman = Console.ReadLine();
+        int tryConvertToInt = 0;
+        if (int.TryParse(Roman, out tryConvertToInt))
+        {
+            if ((tryConvertToInt < 0) || (tryConvertToInt > 4000))
+            {
+                Console.WriteLine("insert value betwheen 1 and 4000");
+            }
+            else
+            {
+                Roman = Main.Roman.ToRoman(tryConvertToInt);
+                Console.WriteLine(Roman);
+            }
+        }
+        else
+        {
+            Console.WriteLine(Main.Roman.FromRoman(Roman));
+        }
+        break;
+
+    case 4:
+        //link https://www.codewars.com/kata/54b72c16cd7f5154e9000457
+        string azcMorse = ".. -   .-. .- .. -. . -..   --- -. .   ... " +
+            "..- -- -- . .-.   -.. .- -.-- ......   .... .   .-- .- .." +
+            ".   ...- . .-. -.--   ... - .-. --- -. --.";
+        string oneZeroMorse = "110011001100110000001100000011111100110" +
+            "011111100111111000000000000001100111111001111110011111100" +
+            "0000110011001111110000001111110011001100000011";
+        int timeUnit = 2;
+        Console.WriteLine(Main.DecodeMorse.Katasix(Main.DecodeMorse.KatasixOneZero(oneZeroMorse, timeUnit)));
+        break;
+
+    case 5:
+        // link: https://www.codewars.com/kata/5659c6d896bc135c4c00021e
+        string num = "1";
+        Console.WriteLine(Main.NextSmailerNum.nextSmailer(num));
+        break;
+}
---
+++ classes.cs
@@ -0,0 +1,329 @@
+using Google.Protobuf.WellKnownTypes;
+using MySqlX.XDevAPI.Common;
+using Org.BouncyCastle.Asn1.Cms;
+using Org.BouncyCastle.Asn1.Crmf;
+using Org.BouncyCastle.Crypto.Generators;
+using System;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Diagnostics.SymbolStore;
+using System.Globalization;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
+using static Main;
+
+public static class Main
+{
+    public static int Sol(int first)
+    {
+        // If we list all the natural numbers below 10 that are multiples of 3 or 5,
+        // we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+        //This is used if the user himself wants to enter a value
+        int ifirst = Convert.ToInt32(first);
+        int i = 0;
+        int res = 0;
+        // Set limits
+        if (ifirst > 1000) Console.WriteLine($"Number {ifirst} too big");
+        if (ifirst <= 0) Console.WriteLine("0");
+        if (ifirst <= 1000 || ifirst >= 1)
+        {
+            do
+            {
+                // for number "3"
+                i = i + 3;
+                res = res + i;
+            }
+            while (i < ifirst);
+            i = 0;
+            do
+            {
+                // for number "5"
+                i = i + 5;
+                res = res + i;
+            }
+            while (i < ifirst);
+            i = 0;
+            do
+            {
+                // if the number is repeated
+                res = res - 15;
+                i = i + 15;
+            }
+            while (i < ifirst);
+        }
+        return res;
+    }
+
+    public static string Upper(string text)
+    {
+        //This function raises the first letter of each word
+
+        TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+        string res = (myTI.ToTitleCase(text));
+
+        return res;
+    }
+    public static class Times
+    {
+        // This class is for converting seconds to days, hours, minutes and seconds
+        public static int[] TimeScore(int timeSecond)
+        {
+            int id = 0;
+            int ih = 0;
+            int im = 0;
+            int ts = timeSecond;
+            if (ts >= 86400)
+            {
+                do
+                {
+                    ts = ts - 86400;
+                    id = id + 1;
+                }
+                while (ts >= 86400);
+            }
+
+            if (ts >= 3600)
+            {
+                do
+                {
+                    ts = ts - 3600;
+                    ih = ih + 1;
+                }
+                while (ts >= 3600);
+            }
+            if (ts >= 60)
+            {
+                do
+                {
+                    ts = ts - 60;
+                    im = im + 1;
+                }
+                while (ts >= 60);
+            }
+            int[] result = { id, ih, im, ts };
+            return result;
+        }
+    }
+    public static class Roman
+    {
+        // This class is converting to/from Roman numerals
+        public static string ToRoman(int number)
+        {
+            if (number < 1) return string.Empty;
+            if (number >= 1000) return "M" + ToRoman(number - 1000);
+            if (number >= 900) return "CM" + ToRoman(number - 900);
+            if (number >= 500) return "D" + ToRoman(number - 500);
+            if (number >= 400) return "CD" + ToRoman(number - 400);
+            if (number >= 100) return "C" + ToRoman(number - 100);
+            if (number >= 90) return "XC" + ToRoman(number - 90);
+            if (number >= 50) return "L" + ToRoman(number - 50);
+            if (number >= 40) return "XL" + ToRoman(number - 40);
+            if (number >= 10) return "X" + ToRoman(number - 10);
+            if (number >= 9) return "IX" + ToRoman(number - 9);
+            if (number >= 5) return "V" + ToRoman(number - 5);
+            if (number >= 4) return "IV" + ToRoman(number - 4);
+            if (number >= 1) return "I" + ToRoman(number - 1);
+            throw new ArgumentOutOfRangeException("something bad happened");
+        }
+
+        public static int FromRoman(string number)
+        {
+            int num = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (i + 1 < number.Length && RomanMap[number[i]] < RomanMap[number[i + 1]])
+                {
+                    num -= RomanMap[number[i]];
+                }
+                else
+                {
+                    num += RomanMap[number[i]];
+                }
+            }
+            return num;
+        }
+
+        private static Dictionary<char, int> RomanMap = new Dictionary<char, int>()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+    }
+
+    public static class DecodeMorse
+    {
+        // Decodes Morse code
+        public static string Katasix(string forDecode)
+        {
+            string result = "";
+            forDecode = forDecode.Replace("   ", " _ ");
+            string[] text = forDecode.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < text.Length; i++)
+            {
+                result += MorseToAbc[text[i]];
+            }
+            return result;
+        }
+
+        public static string KatasixOneZero(string forDecodeOneZero, int unit)
+        {
+            // Deciphers Morse code from zeros and ones
+            string result = "";
+            if (unit == 2)
+            {
+                forDecodeOneZero = forDecodeOneZero.Replace("00000000000000", "   ");
+                forDecodeOneZero = forDecodeOneZero.Replace("111111", "-");
+                forDecodeOneZero = forDecodeOneZero.Replace("11", ".");
+                forDecodeOneZero = forDecodeOneZero.Replace("000000", " ");
+                forDecodeOneZero = forDecodeOneZero.Replace("00", "");
+            }
+            else
+            {
+                forDecodeOneZero = forDecodeOneZero.Replace("0000000", "   ");
+                forDecodeOneZero = forDecodeOneZero.Replace("111", "-");
+                forDecodeOneZero = forDecodeOneZero.Replace("1", ".");
+                forDecodeOneZero = forDecodeOneZero.Replace("000", " ");
+                forDecodeOneZero = forDecodeOneZero.Replace("0", "");
+            }
+            return forDecodeOneZero;
+        }
+
+        private static Dictionary<string, string> MorseToAbc = new Dictionary<string, string>()
+        {
+            {".-", "A"},
+            {"-...", "B"},
+            {"-.-.", "C"},
+            {"-..", "D"},
+            {".", "E"}, 
+            {"..-.", "F"}, 
+            {"--.", "G"}, 
+            {"....", "H"},
+            {"..", "I"}, 
+            {".---", "J"}, 
+            {"-.-", "K"}, 
+            {".-..", "L"},
+            {"--", "M"}, 
+            {"-.", "N"}, 
+            {"---", "O"}, 
+            {".--.", "P"},
+            {"--.-", "Q"}, 
+            {".-.", "R"}, 
+            {"...", "S"}, 
+            {"-", "T"},
+            {"..-", "U"}, 
+            {"...-", "V"}, 
+            {".--", "W"}, 
+            {"-..-", "X"},
+            {"-.--", "Y"},
+            {"--..", "Z"},
+            {".----", "1"},
+            {"..---", "2"},
+            {"...--", "3"},
+            {"....-", "4"},
+            {".....", "5"},
+            {"-....", "6"},
+            {"--...", "7"},
+            {"---..", "8"},
+            {"----.", "9"},
+            {"-----", "0"},
+            {"_", " " },
+            {"......", "." },
+        };
+    }
+
+    public static class NextSmailerNum
+    {
+        public static string nextSmailer(string num)
+        {
+            int inum = Convert.ToInt32(num);
+            if (inum < 10) return "-1";
+            else if (CheckSameDigit(inum) == 1) return "-1";
+            else
+            {
+                string result = "";
+                int[] numbers = new int[num.Length];
+                for (int i = numbers.Length - 1; i >= 0; i--)
+                {
+                    for (int y = 0; y < num.Length; y++)
+                    {
+                        if (Char.IsDigit(num[i]))
+                        {
+                            numbers[y] = IntForChar(num[y]);
+                        }
+                    }
+                    int a = numbers[i];
+                    numbers[i] = numbers[i - 1];
+                    numbers[i - 1] = a;
+
+                    foreach (var item in numbers)
+                    {
+                        result += (item.ToString());
+                    }
+                    if (Convert.ToInt32(result) < Convert.ToInt32(num))
+                    {
+                        if (numbers[0] == 0)
+                        {
+                            result = "";
+                            int tmp = numbers[numbers.Length - 1];
+                            numbers[numbers.Length - 1] = numbers[0];
+                            numbers[0] = tmp;
+                            foreach (var items in numbers)
+                            {
+                                result += (items.ToString());
+                            }
+                            return result;
+                        }
+                        else return result;
+                    }
+                    result = "";
+                    if (i <= 1) return "-1";
+                }
+                return "-1";
+            }
+
+            //int a, k, e, c;
+            //a = Convert.ToInt32(Console.ReadLine());
+            //k = a % 10;
+            //c = a / 10 % 10;
+            //e = a / 10 / 10 % 10;
+            //int res = Math.Min(Math.Min((k * 100 + c * 10 + e), (k * 100 + e * 10 + c)),
+            //    Math.Min(Math.Min((e * 100 + k * 10 + c),(c * 100 + k * 10 + e)),
+            //    Math.Min((c * 100 + e * 10 + k), a)));
+            //Console.WriteLine(res);
+        }
+        static int IntForChar(char ch)
+        {
+            switch (ch)
+            {
+                case '0': return 0;
+                case '1': return 1;
+                case '2': return 2;
+                case '3': return 3;
+                case '4': return 4;
+                case '5': return 5;
+                case '6': return 6;
+                case '7': return 7;
+                case '8': return 8;
+                case '9': return 9;
+            }
+            return -1; // если это не число
+        }
+
+        public static int CheckSameDigit(int num)
+        {
+            int digit = num % 10;
+            while (num > 0)
+            {
+                if (num % 10 != digit) return 0;
+                num = num / 10;
+            }
+            return 1;
+        }
+    }
+}
